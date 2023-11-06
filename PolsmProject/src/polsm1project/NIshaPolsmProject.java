ae8142337cd8f216a2d8f6535967389cf163363f
Nisha Sharma <124479532+hhggfdsds@users.noreply.github.com>
2023-11-05 16:56:00 -0800

    webhook project

--- PolsmProject/src/polsm1project/NIshaPolsmProject.java
+++ PolsmProject/src/polsm1project/NIshaPolsmProject.java
@@ -7,6 +7,8 @@ public class NIshaPolsmProject {
 		System.out.println("Sending code in git ");
 		System.out.println("Welcome new polsm project");
 		System.out.println(23*32);
+		System.out.println(65+54);
+		System.out.println(23+98);
 		
 
 	}
